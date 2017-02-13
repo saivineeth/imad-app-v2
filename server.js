7e997d17d0e6ebc11a7c8e7df67fab6670211468
saivineeth <saivineeth@users.noreply.github.com>
2017-02-14 00:34:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ head:`article3`
 
 function sende(aname){
 var arti = aname.name;
-
+console.log(Object.keys(aname.name));
 var he = aname.head;
 var para = aname.paragraph;
 var articleTemp=`<!DOCTYPE html>
