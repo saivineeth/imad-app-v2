d64964bd6c0bb136d2e445beaac874816a19e9ea
saivineeth <saivineeth@users.noreply.github.com>
2017-02-13 21:46:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/a.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'a.jpg'));
 });
+app.get('/ui/b.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'b.jpg'));
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
