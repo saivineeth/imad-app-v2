c4f3018b3c627501d20a8d801889e688d3efd512
saivineeth <saivineeth@users.noreply.github.com>
2017-02-13 23:27:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,57 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var article1 = {
+arti_name:'article-one',
+paragraph:`This is my first article`,
+head:`article1`
+};
+var article2 = {
+arti_name:'article-two',
+paragraph:`This is my second article`,
+head:`article2`
+};
+var article3 = {
+arti_name:'article-two',
+paragraph:`This is my third article`,
+head:`article3`
+};
+
+function send(sud){
+var arti=sudo.arti_name;
+var he=sudo.head;
+var para=sudo.paragraph;
+var article=`<!DOCTYPE html>
+<html>
+<head>
+<title>Saivineeth | ${arti}</title>
+<link href="style-arti.css" rel="stylesheet">	
+</head>
+<body>
+
+	<div class="content">
+    <h1 class="title">${he}</h1>
+	<p> ${para}
+	</p>
+	</div>
+	</body>
+</html>`;
+    return article;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/:articleName', function (req, res) {
+ var article=req.params.articleName;
+  res.send(send(article));
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+app.get('/ui/style-arti.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style-arti.css'));
 });
 
 app.get('/ui/a.jpg', function (req, res) {
